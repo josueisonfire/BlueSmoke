530df9dd31e06c151131c1964323eafb8f747571
nuwan <nuwanwre@live.com>
2017-06-01 00:23:27 +0900

    Rolled back path changes

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 #													APP ROUTES
 # ===============================================================
 
-@app.route('.')
+@app.route('/')
 def login():
     return render_template('index.html')
 	
