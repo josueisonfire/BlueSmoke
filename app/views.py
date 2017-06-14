033849a23b0e43521c6b26d910f5d7f467b0a13d
nuwan <nuwanwre@live.com>
2017-06-15 01:30:12 +0900

    Testing index route

--- app/views.py
+++ app/views.py
@@ -38,7 +38,8 @@ from werkzeug import generate_password_hash, check_password_hash
 # Starting point
 @app.route('/')
 def login():
-	return render_template('index.html', title = 'Please enter device ID to continue')
+	return "hello"
+	#return render_template('index.html', title = 'Please enter device ID to continue')
 
 # Route to validate a device
 @app.route('/validateDevice', methods = ['POST', 'GET'])
