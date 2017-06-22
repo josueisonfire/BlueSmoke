0628ca6a93af6ba6134ebedbc7e4a6b5f8a758b6
nuwan <nuwanwre@live.com>
2017-06-22 20:12:02 +0900

    Background process to listen

---
+++ worker.py
@@ -0,0 +1,41 @@
+'''
+
+	This app defines the worker process to communicate with the 
+	device. It will be running continually in the background.
+	
+	$PORT to be specified
+	
+	
+	@author 	: 	nuwanwre via blueSmoke. C412
+	@version 	: 	1.0.0
+	
+'''
+
+import os
+import logging
+# import redis
+import gevent
+from flask import Flask, render_template
+from flask_sockets import Sockets
+
+# REDIS_URL = os.environ['REDIS_URL']
+# REDIS_CHAN = 'device'
+
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+
+@sockets.route('/listen')
+def listen(ws):
+	# Recieve incoming traffic
+	while not ws.closed:
+		# gevent.sleep(0.1)
+		message = ws.receive()
+		
+		if message:
+			app.logger.info(u'Inserting message  : {}'.format(message))
+		
+
+if __name__ == '__main__':
+	app.run(debug = True)
