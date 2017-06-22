0ed7f63b57b4346a00ad6904dd1378e141e6d835
nuwan <nuwanwre@live.com>
2017-06-22 23:25:33 +0900

    set port to run

--- run.py
+++ run.py
@@ -10,7 +10,9 @@
 
 #!flask/bin/python
 from app import app
+from worker import worker
 
 # Encapsulate Run
 if __name__ = '__main__':
-	app.run(debug = True)
+	port = int(os.environ.get("PORT", 5001))
+	app.run(debug = True, host = "0.0.0.0", port = port)
